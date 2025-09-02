744e343769104055961bb039b450c33f19332479
hsweif <hsweif@icloud.com>
2025-09-02 14:39:22 +0800

    refactor: update hop.nvim configuration
    - Remove f key mapping for hop to restore native vim find behavior
    - Keep <leader>sf for hop word navigation
    - Maintain color scheme integration for hop highlights

--- lua/config/nvim_hop.lua
+++ lua/config/nvim_hop.lua
@@ -1,37 +1,28 @@
 local keymap = vim.keymap
 local hop = require("hop")
 hop.setup {
-  case_insensitive = true,
-  char2_fallback_key = "<CR>",
-  quit_key = "<Esc>",
-  match_mappings = { "zh_sc" },
+    case_insensitive = true,
+    char2_fallback_key = "<CR>",
+    quit_key = "<Esc>",
+    match_mappings = { "zh_sc" },
 }
-
-keymap.set({ "n", "v", "o" }, "f", "", {
-  silent = true,
-  noremap = true,
-  callback = function()
-    hop.hint_char2()
-  end,
-  desc = "nvim-hop char2",
-})
 
 -- Add word mode for legacy mapping
 keymap.set("n", "<leader>sf", function()
-  hop.hint_words()
+    hop.hint_words()
 end, {
-  silent = true,
-  noremap = true,
-  desc = "Hop to word",
+    silent = true,
+    noremap = true,
+    desc = "Hop to word",
 })
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = function()
-    vim.cmd([[
+    pattern = "*",
+    callback = function()
+        vim.cmd([[
       hi HopNextKey cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff
       hi HopNextKey1 cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff
       hi HopNextKey2 cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff
     ]])
-  end,
+    end,
 })
